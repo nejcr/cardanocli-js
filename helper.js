802dc5cc9b505366eacb5e39d57fbcd21f028f95
nejcr <ravnik.nejc@gmail.com>
2021-09-13 22:48:18 +0200

    Added empty redeemer22

--- helper.js
+++ helper.js
@@ -217,7 +217,7 @@ exports.mintToString = (dir, minting) => {
   result = result.trim();
   result += `" `;
   const usedScripts = [];
-  const execUni=`"(28365043, 125200)"`
+  const execUni=`"(228365043, 125200)"`
   const redeemerFile = this.jsonToPath(dir,{"constructor":0, "fields":[]})
   result += minting
     .map((mint) => {
